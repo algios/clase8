0f42bce503ce965fbd13117df03a4115e6ced58f
Equipo04-502.A <Equipo04-502.A@168.176.122.115>
2015-03-07 17:40:16 -0500

    Primera version

---
+++ app/controllers/PersonalController.php
@@ -0,0 +1,26 @@
+<?php
+
+class PersonalController extends BaseController{
+    
+    public function getRegistrar($tipo) {
+        if($tipo=="medico"){
+            return View::make('registro.medico');
+                 
+        }elseif ($tipo=='paciente') {
+            return View::make('registro.paciente');
+  
+            
+        }elseif ($tipo=='enfermera') {
+            return View::make('registro.enfermera');
+  
+            
+        }else{
+            echo "Error";
+        }
+    }
+    
+    public function getEditar(){
+        echo "Estoy editando";
+    }
+    
+}
